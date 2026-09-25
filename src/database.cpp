9e61dd389e5725a3276e7fd7db016619d0d4c84c
DeltaProcess <undersealife320320@gmail.com>
2026-09-25 14:38:31 -0500

    fixed database implementation
    -fixed CMakeLists.txt to link a library for database
    -standardized and removed main() from database.cpp

--- src/database.cpp
+++ src/database.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <pqxx/pqxx>
 #include <string>
-using namespace std;
-int main() {
+
+int init() {
 	try {
 		//connects to the database
 		pqxx::connection c("host=/var/run/postgresql port=5432 dbname=photon target_session_attrs=read-write");
-		cout << "Successfully connected to: " << c.dbname() << endl;	
+		std::cout << "Successfully connected to: " << c.dbname() << std::endl;	
 		
 		//starts a query
 		pqxx::work tx(c);
@@ -16,23 +16,22 @@ int main() {
 
         //prints column names
         for (pqxx::row_size_type col = 0; col < res.columns(); col++) {
-            cout << res.column_name(col) << "\t";
+            std::cout << res.column_name(col) << "\t";
         }
-        cout << "\n";
+        std::cout << "\n";
 
         //prints every row
         for (pqxx::result::size_type r = 0; r < res.size(); r++) {
 			for (pqxx::row_size_type c = 0; c < res.columns(); c++) {
-				cout << res[r][c].c_str() << "\t";
+				std::cout << res[r][c].c_str() << "\t";
 			}
-			cout << "\n";
+			std::cout << "\n";
 		}
         
-       // tx.commit(); if writing to database, need this line
-    
-	}catch (const exception &e) {
-		cerr << e.what() << endl;
+        // tx.commit(); if writing to database, need this line
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
 		return 1;
 	}
-	
+	return 0;
 }
