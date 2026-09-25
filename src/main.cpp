a94000675724cd90d45ffa2aba3e768240f0a148
willrwhitlow <willrwhitlow@gmail.com>
2026-09-25 02:42:08 -0500

    Added CLI Network Option in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include "interface.h"
+#include <iostream>
+#include <string>
 
 int main(int argc, char** argv)
 {
+     std::string networkAddress;
+
+     std::cout << "Type the network address you'd like to use: ";
+     std::cin >> networkAddress;
+     std::cout << "Using network: " << networkAddress << std::endl;
+     
      Interface* interface = new Interface(argv[0]);
+
      return 0;
 }
